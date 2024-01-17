2f08ec9d31d21fed92e940f2c967f172d1e62a6c
Shafnaa <saujanashafi2003@gmail.com>
2024-01-17 17:05:57 +0700

    feat: explore page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,9 @@
-/* eslint react/no-unescaped-entities */
 import Link from "next/link";
 
 export default function NotFound() {
   return (
     <div className="h-full min-h-screen w-full flex flex-col gap-3 justify-center items-center text-center p-4">
-      <h1 className="font-bold text-xl">Not found - 404!</h1>
+      <h1 className="font-bold text-xl">Not found – 404!</h1>
       <p>
         I'm so sorry there must have been a mistake or we are currently still
         working this feature. <br /> Thank you for your understanding! ❤️
